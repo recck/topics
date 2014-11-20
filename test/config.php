5ca3603a66ff06254d3135feddb3548f1ff6c058
mrecck <mar1491@rit.edu>
2014-11-20 18:52:50 -0500

    started config

---
+++ test/config.php
@@ -0,0 +1,17 @@
+<?php
+phpinfo();
+$info = parse_ini_file("../config.ini", true);
+$db = $info['db_info'];
+
+$dsn = sprintf("host=%s port=%d dbname=%s user=%s password=%s",
+        $db['db_host'],
+        $db['db_port'],
+        $db['db_name'],
+        $db['db_user'],
+        $db['db_pass']);
+
+$connections = array(
+    'unsafe' => pg_connect($dsn),
+    'safe' => new PDO('pgsql:' . $dsn)
+);
+
