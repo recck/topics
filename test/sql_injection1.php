3a43cf40f8d0bb4d1b161f2849edb3ff171aab9c
mrecck <mar1491@rit.edu>
2014-11-20 17:43:07 -0500

    tabs to spaces

--- test/sql_injection1.php
+++ test/sql_injection1.php
@@ -1,25 +1,25 @@
 <!doctype html>
 <html>
 <head>
-	<title>Vulnerability Test 1</title>
-	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" type="text/css" />
+    <title>Vulnerability Test 1</title>
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" type="text/css" />
 </head>
 <body>
-	<div class="container">
-		<div class="row">
-			<div class="col-lg-6 col-md-8 col-sm-12 col-lg-offset-3 col-md-offset-2">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 col-md-8 col-sm-12 col-lg-offset-3 col-md-offset-2">
                 <div class="page-header">
                     <h2>login</h2>
                 </div>
-				<form method="post" class="form-horizontal">
-					<div class="form-group">
-						<label class="col-sm-3 control-label" for="un">
-						Username
-						</label>
-						<div class="col-sm-9">
-							<input type="text" id="un" name="un" class="form-control" />
-						</div>
-					</div>
+                <form method="post" class="form-horizontal">
+                    <div class="form-group">
+                        <label class="col-sm-3 control-label" for="un">
+                        Username
+                        </label>
+                        <div class="col-sm-9">
+                            <input type="text" id="un" name="un" class="form-control" />
+                        </div>
+                    </div>
                     <div class="form-group">
                         <label class="col-sm-3 control-label" for="pw">
                         Password
@@ -35,9 +35,9 @@
                             </button>
                         </div>
                     </div>
-				</form>
-			</div>
-		</div>
-	</div>
+                </form>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
